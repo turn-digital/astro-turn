437600ecfdbba9c671031c145d0cf2145bfa0174
Artem Vovk <artem@turn.lv>
2023-03-24 13:41:55 +0200

    add modal header mob

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,24 +4,6 @@ import { t, changeLanguage } from "i18next";
 changeLanguage("lv");
 
 const Footer = () => {
-  const footer = {
-    informationHeader: t("global.FooterInformationHeader"),
-    cookiePolicy: t("global.FooterCookiePolicy"),
-    availability: t("global.FooterAvailability"),
-    privacyPolicy: t("global.FooterPrivacyPolicy"),
-    contactsHeader: t("global.FooterContactsHeader"),
-    phone: t("global.FooterPhone"),
-    email: t("global.FooterEmail"),
-    address: t("global.FooterAddress"),
-    companyDetails: t("global.FooterCompanyDetailsHeader"),
-    SIA: t("global.FooterSIA"),
-    REG: t("global.FooterREG"),
-    BANK: t("global.FooterBANK"),
-    SWIFT: t("global.FooterSWIFT"),
-    ACCOUNT: t("global.FooterACCOUNT"),
-    copyright: t("global.FooterCopyright"),
-};
-
   return (
     <>
       <footer className='footer'>
@@ -38,43 +20,43 @@ const Footer = () => {
 
           <ul className='footer__list'>
             <li className='footer__item'>
-              <h5 className="footer__title">{footer.informationHeader}</h5>
+              <h5 className="footer__title">{t("global.FooterInformationHeader")}</h5>
               <div>
-                <a className='footer__text footer__text--hover' href="/">{footer.cookiePolicy}</a>
-                <a className='footer__text footer__text--hover' href="/">{footer.availability}</a>
-                <a className='footer__text footer__text--hover' href="/">{footer.privacyPolicy}</a>
+                <a className='footer__text footer__text--hover' href="/">{t("global.FooterCookiePolicy")}</a>
+                <a className='footer__text footer__text--hover' href="/">{t("global.FooterAvailability")}</a>
+                <a className='footer__text footer__text--hover' href="/">{t("global.FooterPrivacyPolicy")}</a>
               </div>
             </li>
             <li className='footer__item'>
-              <h5 className="footer__title">{footer.contactsHeader}</h5>
+              <h5 className="footer__title">{t("global.FooterContactsHeader")}</h5>
               <ul>
                 <li>
-                  <a className='footer__text footer__text--hover' href={"tel:" + footer.phone}>{footer.phone}</a>
+                  <a className='footer__text footer__text--hover' href={"tel:" + t("global.FooterPhone")}>{t("global.FooterPhone")}</a>
                 </li>
                 <li>
-                  <a className='footer__text footer__text--hover' href={"mailto:" + footer.email}>{footer.email}</a>
+                  <a className='footer__text footer__text--hover' href={"mailto:" + t("global.FooterEmail")}>{t("global.FooterEmail")}</a>
                 </li>
                 <li>
-                  <a className='footer__text footer__text--hover' href=''>{footer.address}</a>
+                  <a className='footer__text footer__text--hover' href=''>{t("global.FooterAddress")}</a>
                 </li>
               </ul>
             </li>
             <li className='footer__item'>
-              <h5 className="footer__title">{footer.companyDetails}</h5>
+              <h5 className="footer__title">{t("global.FooterCompanyDetailsHeader")}</h5>
               <ul>
                 <li className='footer__text'>
-                  <p className='footer__inline'>{footer.SIA}&nbsp;</p>
-                  <p className='footer__inline'>{footer.REG}&nbsp;</p>
+                  <p className='footer__inline'>{t("global.FooterSIA")}&nbsp;</p>
+                  <p className='footer__inline'>{t("global.FooterREG")}&nbsp;</p>
                 </li>
                 <li className='footer__text'>
-                  <p className='footer__inline'>{footer.BANK}&nbsp;</p>
-                  <p className='footer__inline'>{footer.SWIFT}</p>
+                  <p className='footer__inline'>{t("global.FooterBANK")}&nbsp;</p>
+                  <p className='footer__inline'>{t("global.FooterSWIFT")}</p>
                 </li>
-                <li className='footer__text'>{footer.ACCOUNT}</li>
+                <li className='footer__text'>{t("global.FooterACCOUNT")}</li>
               </ul>
             </li>
           </ul>
-          <p  className='footer__text'>{footer.copyright}</p>
+          <p  className='footer__text'>{t("global.FooterCopyright")}</p>
         </div>
       </footer>
     </>
