b7b1a318971ee87b0e7169336710059f936ccf11
Vladyslav Tkachenko <vlad@wtf.lv>
2023-04-18 17:01:55 +0300

    changing site url and commenting base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
-import image from '@astrojs/image';
+import image from "@astrojs/image";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://turn-digital.github.io',
-  base: '/astro-turn',
-  integrations: [astroI18next(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), preact()]
+  site: "http://turn.turn.lv/",
+  // base: "/astro-turn",
+  integrations: [
+    astroI18next(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    preact(),
+  ],
 });
