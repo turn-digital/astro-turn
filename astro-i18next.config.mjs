726a470d0a00a6f7b1301bd43c2947edb0c19a21
vladislavs1 <61745007+vladislavs1@users.noreply.github.com>
2025-10-02 13:20:47 +0300

    Update astro-i18next.config.mjs
    update translations config

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
-    defaultLocale: "lv",
-    locales: ["lv"],
-  };
+  defaultLocale: "lv",
+  locales: ["lv"],
+  i18next: {
+    ns: ["translation"],        // we have just one namespace
+    defaultNS: "translation",
+    backend: {
+      // IMPORTANT: no leading slash
+      loadPath: "locales/{{lng}}/{{ns}}.json",
+    },
+  },
+};
